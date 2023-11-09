798dd18b91bdcbe3c3f9af4ede89a46012e6e584
Manu Ferrer <38438207+ManuFerrer094@users.noreply.github.com>
2023-11-09 09:49:18 +0100

    Update functions.php

--- json-formatter/functions.php
+++ json-formatter/functions.php
@@ -12,11 +12,67 @@ add_action('wp_enqueue_scripts', 'load_json_formatter_scripts');
 function json_formatter_shortcode() {
     ob_start();
     ?>
-    <section class="json-formatter-section">
+ <section class="json-formatter-section">
         <div class="container">
-            <!-- Contenido HTML aquí -->
+            <div class="step">
+                <h4>Drag and drop your JSON here or write your code.</h4>
+            </div>
+            <div class="first-step">
+                <div class="textarea-container">
+                    <textarea id="json-input" placeholder="Drag and drop your JSON file here or write your code" class="json-textarea" ondrop="handleDrop(event)" ondragover="handleDragOver(event)"></textarea>
+                </div>
+                <div class="process-button-container">
+                    <div class="select-container-1">
+                        <h5>Formatting Style</h5>
+                        <select id="format-select" class="button-panel">
+                            <option value="0">Compact</option>
+                            <option value="1">1 Space Tab</option>
+                            <option value="2">2 Space Tab</option>
+                            <option value="3">3 Space Tab</option>
+                            <option value="4">4 Space Tab</option>
+                        </select>
+                    </div>
+                    <div class="select-container-2">
+                        <h5>Validation Type</h5>
+                        <select id="validation-type" class="button-panel">
+                            <option value="ecma-404">ECMA-404</option>
+                            <option value="rfc-8259">RFC-8259</option>
+                            <option value="rfc-7159">RFC-7159</option>
+                            <option value="rfc-4627">RFC-4627</option>
+                            <option value="skip-validation">Skip Validation</option>
+                        </select>
+                    </div>
+                    <div class="select-container-3">
+                        <h5>Press Process Button</h5>
+                        <button class="button-panel" id="format-button">Process JSON</button>
+                    </div>
+                </div>
+            </div>
+            <div class="step">
+                <h4>The Processed JSON Result.</h4>
+            </div>
+            <div class="first-step">
+                <div class="textarea-container">
+                    <textarea id="formatted-json" class="json-textarea"></textarea>
+                </div>
+                <div class="process-button-container">
+                    <div class="select-container-1">
+                        <h5>Download result</h5>
+                        <button class="button-panel" id="download-button">Download</button>
+                    </div>
+                    <div class="select-container-3">
+                        <h5>Copy result</h5>
+                        <button class="button-panel" id="copy-button">Copy to Clipboard</button>
+                    </div>
+                    <div class="select-container-2">
+                        <h5>Clear result</h5>
+                        <button class="button-panel" id="clear-button">Clear</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
+    <script src="<?php echo plugin_dir_url(__FILE__) . 'js/json-formatter.js'; ?>"></script>
     <?php
     return ob_get_clean();
 }
